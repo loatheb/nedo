a2a48154307ab167cb3f827e68fbc685a5dbcb75
loatheb <loatheb.zhao@gmail.com>
2018-07-08 18:06:29 +0800

    fix(testcase): fix the raw content of module url in testcase

--- __test/case/code2.js
+++ __test/case/code2.js
@@ -1,4 +1,4 @@
-const snippet = require("http://raw.githubusercontent.com/loatheb/require-enhancer/master/__test/case/code.js");
+const snippet = require("https://raw.githubusercontent.com/loatheb/nedo/master/__test/case/code.js");
 
 const str = "hello code2.js";
 module.exports = {
